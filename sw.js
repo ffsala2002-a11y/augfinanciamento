3b10d318c780d44d8b488f555ecb13a4ebf516ea
ffsala2002-a11y <ffsala2002@gmail.com>
2026-06-15 01:05:57 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = "aug-v3";
+
+const FILES = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./src/img/copa26-v3.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
+  );
+
+  self.skipWaiting();
+});
+
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
+});
